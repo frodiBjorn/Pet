3754cba2f07f88a388239e2314fa51aa27398a8a
Ivan Fedotovsky <fedotovsky96@gmail.com>
2021-07-01 12:22:05 +0400

    21 add something to dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,33 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className = {s.dialogs}>
+            <div className = {s.dialogsItems}>
+                <div className = {s.dialog + " " + s.active}>
+                    Ivan
+                </div>
+                <div className = {s.dialog}>
+                    Maskim
+                </div>
+                <div className = {s.dialog}>
+                    Svyatoslav
+                </div>
+                <div className = {s.dialog}>
+                    Valeria
+                </div>
+                <div className = {s.dialog}>
+                    Sveta
+                </div>
+                <div className = {s.dialog}>
+                    John
+                </div>
+
+            </div>
+            <div className = {s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>What is your name?</div>
+            </div>
         </div>
     )
 }
